a58af942a5a27d28a99198ca2c19bd73044bf2d1
Taichi Uragami <backpaper0@gmail.com>
2015-11-20 22:20:38 +0900

    @Produces漏れとった

--- jersey-sandbox/src/main/java/addition/Addition.java
+++ jersey-sandbox/src/main/java/addition/Addition.java
@@ -2,11 +2,13 @@ package addition;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 @Path("/addition")
 public class Addition {
     @GET
+    @Produces("text/plain")
     public int calculate(@QueryParam("a") int a, @QueryParam("b") int b) {
         return a + b;
     }
