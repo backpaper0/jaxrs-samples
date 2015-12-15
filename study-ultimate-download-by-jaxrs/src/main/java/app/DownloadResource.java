f07d7d1613704f8c966d8c573bccd70d0b7ef305
Taichi Uragami <backpaper0@gmail.com>
2015-12-16 05:21:51 +0900

    日本語でファイル名を付ける

--- study-ultimate-download-by-jaxrs/src/main/java/app/DownloadResource.java
+++ study-ultimate-download-by-jaxrs/src/main/java/app/DownloadResource.java
@@ -1,15 +1,29 @@
 package app;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
 
 @Path("download")
 public class DownloadResource {
 
     @GET
     @Produces("application/force-download")
-    public String download() {
-        return "hello world";
+    public Response download() {
+        try {
+            return Response.ok("hello world").header(
+                    HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\""
+                            + URLEncoder.encode("サンプル.txt", "UTF-8") + "\"")
+                    .build();
+        } catch (UnsupportedEncodingException e) {
+            throw new InternalServerErrorException(e);
+        }
     }
 }
