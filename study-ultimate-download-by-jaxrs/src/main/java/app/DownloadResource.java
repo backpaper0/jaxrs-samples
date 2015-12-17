6fb6172d6a43f2d9522fd9b76f3711f79b399697
Taichi Uragami <backpaper0@gmail.com>
2015-12-17 23:29:12 +0900

    一時ファイルを経由する

--- study-ultimate-download-by-jaxrs/src/main/java/app/DownloadResource.java
+++ study-ultimate-download-by-jaxrs/src/main/java/app/DownloadResource.java
@@ -1,21 +1,41 @@
 package app;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
-@Path("download")
+@javax.ws.rs.Path("download")
 public class DownloadResource {
 
     @GET
     @Produces("application/force-download")
     public Response download() {
-        String filename = "サンプル.txt";
-        return Response.ok("hello world")
-                .header(HttpHeaders.CONTENT_DISPOSITION,
-                        "attachment; filename=\"" + filename + "\"")
-                .build();
+        try {
+            Path tempFile = Files.createTempFile("download-", ".tmp");
+            try (BufferedWriter out = Files.newBufferedWriter(tempFile)) {
+                out.write("hello world");
+            }
+
+            String filename = "サンプル.txt";
+            StreamingOutput so = out -> {
+                Files.copy(tempFile, out);
+                Files.delete(tempFile);
+            };
+            return Response.ok(so)
+                    .header(HttpHeaders.CONTENT_LENGTH, Files.size(tempFile))
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            "attachment; filename=\"" + filename + "\"")
+                    .build();
+        } catch (IOException e) {
+            throw new InternalServerErrorException(e);
+        }
     }
 }
