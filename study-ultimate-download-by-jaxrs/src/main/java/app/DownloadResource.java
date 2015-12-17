500dbf7f84298573456ddac8782d26e23d436de8
Taichi Uragami <backpaper0@gmail.com>
2015-12-17 22:56:18 +0900

    エンコード処理をフィルターに分けた

--- study-ultimate-download-by-jaxrs/src/main/java/app/DownloadResource.java
+++ study-ultimate-download-by-jaxrs/src/main/java/app/DownloadResource.java
@@ -1,13 +1,6 @@
 package app;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-import javax.mail.internet.MimeUtility;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.HttpHeaders;
@@ -18,22 +11,11 @@ public class DownloadResource {
 
     @GET
     @Produces("application/force-download")
-    public Response download(
-            @HeaderParam(HttpHeaders.USER_AGENT) @DefaultValue("") String userAgent) {
-        try {
-            String filename0 = "サンプル.txt";
-            String filename;
-            if (userAgent.contains("Gecko")) {
-                filename = MimeUtility.encodeText(filename0, "UTF-8", "B");
-            } else {
-                filename = URLEncoder.encode(filename0, "UTF-8");
-            }
-            return Response.ok("hello world")
-                    .header(HttpHeaders.CONTENT_DISPOSITION,
-                            "attachment; filename=\"" + filename + "\"")
-                    .build();
-        } catch (UnsupportedEncodingException e) {
-            throw new InternalServerErrorException(e);
-        }
+    public Response download() {
+        String filename = "サンプル.txt";
+        return Response.ok("hello world")
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + filename + "\"")
+                .build();
     }
 }
---
+++ study-ultimate-download-by-jaxrs/src/main/java/app/FileNameEncoder.java
@@ -0,0 +1,49 @@
+package app;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import javax.mail.internet.MimeUtility;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class FileNameEncoder implements ContainerResponseFilter {
+
+    private static final String PREFIX = "filename=\"";
+    private static final String SUFFIX = "\"";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+            ContainerResponseContext responseContext) throws IOException {
+        String cd = responseContext
+                .getHeaderString(HttpHeaders.CONTENT_DISPOSITION);
+        if (cd == null) {
+            return;
+        }
+        int start = cd.indexOf(PREFIX);
+        if (start < 0) {
+            return;
+        }
+        start += PREFIX.length();
+        int end = cd.indexOf(SUFFIX, start);
+        if (end < 0) {
+            return;
+        }
+        String rawFileName = cd.substring(start, end);
+        String fileName;
+        String userAgent = responseContext
+                .getHeaderString(HttpHeaders.USER_AGENT);
+        if (userAgent != null && userAgent.contains("Gecko")) {
+            fileName = MimeUtility.encodeText(rawFileName, "UTF-8", "B");
+        } else {
+            fileName = URLEncoder.encode(rawFileName, "UTF-8");
+        }
+        cd = cd.substring(0, start) + fileName + cd.substring(end);
+        responseContext.getHeaders().putSingle(HttpHeaders.CONTENT_DISPOSITION,
+                cd);
+    }
+}
